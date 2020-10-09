42857df72602539f69d86de50e8d2f204d126e01
shaikha-305 <shaikhaaljenaidel@gmail.com>
2020-10-09 21:13:37 +0300

    officially DONE

--- DAMA2/Models.swift
+++ DAMA2/Models.swift
@@ -7,29 +7,20 @@
 //
 
 import Foundation
-func fillTheArray(){
-    for i in 1...64{
-        checkers.append(Checker(location: i, type: .none))
-    }
-    for b in 9...24{
-        checkers[b-1].type = .black
-    }
-    for w in 41...56{
-        checkers[w-1].type = .white
+
+enum Type{
+    case none, black, white
+    var player: String{
+        switch self{
+        case .none: return " "
+        case .black: return "♟"
+        case .white: return "♙"
+        }
     }
 }
 
-func printTable(){
-    print("------------------------------------------------------------------------")
-    for i in 1...64{
-        if i != 9 && i != 8 && i != 7 && i != 6 && i != 5 && i != 4 && i != 3 && i != 2 && i != 1{
-        print(" \(checkers[i-1].location)   \(checkers[i-1].type.player) |", terminator: "")
-        }else {
-            print(" \(checkers[i-1].location)    \(checkers[i-1].type.player) |", terminator: "")
-        }
-        if i == 8 || i == 16 || i == 24 || i == 32 || i == 40 || i == 48 || i == 56 || i == 64{
-            print("")
-            print("------------------------------------------------------------------------")
-        }
-    }
+struct Checker{
+    var location: Int
+    var type: Type
 }
+
--- DAMA2/main.swift
+++ DAMA2/main.swift
@@ -7,33 +7,47 @@
 //
 
 import Foundation
-enum Type{
-    case none, black, white
-    var player: String{
-        switch self{
-        case .none: return " "
-        case .black: return "♟"
-        case .white: return "♙"
-        }
+func fillTheArray(){
+    for i in 1...64{
+        checkers.append(Checker(location: i, type: .none))
     }
-}
-
-struct Checker{
-    var location: Int
-    var type: Type
+    for b in 9...24{
+        checkers[b-1].type = .white
+    }
+    for w in 41...56{
+        checkers[w-1].type = .black
+    }
 }
 var checkers = [Checker]()
 fillTheArray()
-//printTable()
 var input: Int
 func canMove(player: Int)->Bool{
-    if checkers[player-1].type == .black || checkers[player-1].type == .white{
-        return true
+    if 1...64 ~= player{
+        if checkers[player-1].type == .black || checkers[player-1].type == .white{
+            return true
+        }else{
+            return false
+        }
     }else{
         return false
     }
+    
 }
 
+func printTable(){
+    print("------------------------------------------------------------------------")
+    for i in 1...64{
+        if 1...9 ~= i{
+        print(" \(checkers[i-1].location)    \(checkers[i-1].type.player) |", terminator: "")
+        }else {
+            print(" \(checkers[i-1].location)   \(checkers[i-1].type.player) |", terminator: "")
+        }
+        if i % 8 == 0{
+            print("")
+            print("------------------------------------------------------------------------")
+        }
+    }
+}
 func check(toCheck: [Int])->[Int]{
     var isEmpty = [Int]()
     for i in toCheck{
@@ -48,13 +62,33 @@ func firstAndLastColumn(player: Int)->[Int]{
     var l: Int
     var r: Int
     if player % 8 == 0{
-        f = (player-1)+8
-        l = (player-1)-1
-        return [f+1,l+1]
+        switch checkers[player-1].type {
+        case .black:
+             f = (player-1)-8
+             l = (player-1)-1
+             return [f+1,l+1]
+        case .white:
+            f = (player-1)+8
+            l = (player-1)-1
+            return [f+1,l+1]
+        default:
+            return []
+        }
+    }else if (player-1) % 8 == 0{
+        switch checkers[player-1].type {
+        case .black:
+             f = (player-1)-8
+             r = (player-1)+1
+             return [f+1,r+1]
+        case .white:
+            f = (player-1)+8
+            r = (player-1)+1
+            return [f+1,r+1]
+        default:
+            return []
+        }
     }else{
-        f = (player-1)-8
-        r = (player-1)+1
-        return [f+1,r+1]
+        return []
     }
 }
 func anyOtherChecker(player: Int)->[Int]{
@@ -73,26 +107,34 @@ func anyOtherChecker(player: Int)->[Int]{
         return [f+1,l+1,r+1]
     }
 }
-func finalCheck(isValid: Bool, player: Int)->String{
+func finalCheck(isValid: Bool, player: Int)->(String, [Int]){
     var validInputs = [Int]()
     if isValid{
         if player % 8 == 0 || (player-1) % 8 == 0{
             validInputs.append(contentsOf: check(toCheck: firstAndLastColumn(player: player)))
-            return "\(player) can move to \(validInputs) "
+            return validInputs != [] ? ("\(player) can move to \(validInputs) ", validInputs) : ("this checker cant move :(", validInputs)
+            
         }else{
             validInputs.append(contentsOf: check(toCheck: anyOtherChecker(player: player)))
-            return "\(player) can move to \(validInputs) "
+            return validInputs != [] ? ("\(player) can move to \(validInputs) ", validInputs) : ("this checker cant move :(", validInputs)
         }
     }
-    return "⚠️ your input is invalid"
+    return ("⚠️ your input is invalid", [])
 }
 for _ in 0...64{
     printTable()
     print("👋🏻 Please type a number for a piece to move")
     input = Int(readLine()!)!
-    print(finalCheck(isValid: canMove(player: input), player: input))
+    var validPoints = finalCheck(isValid: canMove(player: input), player: input)
+    print(validPoints.0)
     var movement = Int(readLine()!)!
-    checkers[movement-1].type = checkers[input-1].type
-    checkers[input-1].type = .none
+    for i in validPoints.1{
+        if movement == i{
+            checkers[movement-1].type = checkers[input-1].type
+            checkers[input-1].type = .none
+        }else{
+            print("You cant move this checker :(")
+        }
+    }
 }
 
